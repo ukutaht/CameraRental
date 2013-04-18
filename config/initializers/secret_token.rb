5957499d65cabcaa76f87d79b1d834d1be0a8334
ukutaht <uku.taht@gmail.com>
2013-04-18 15:08:19 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CameraRental::Application.config.secret_token = '2d63fc9a45381c3f1da1051d6650a3f067f50ebdb215d13201f4a64aaecc72f47346d23142200f17edece5091e681d8c300be8aedb30a41c8b76ca8abfb0ac25'
